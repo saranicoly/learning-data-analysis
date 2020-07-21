40b0898e415ec8b55f47e9287bdf066abcec3e3a
Sara Nicoly <saranicoly0@gmail.com>
2020-07-20 22:24:49 -0300

    a palavra que mais aparece

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
-dict_name_age = {"sara":19, "caio": 20, "mayara":21}
+"""dict_name_age = {"sara":19, "caio": 20, "mayara":21}
 for k,v in dict_name_age.items():
     print(k,"tem", v, "anos")
+"""
+
+# a palavra que mais aparece
+
+name = input("Enter file:")
+handle = open(name)
+
+counts = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) +1
+
+bigcount = None
+bigword = None
+for word,count in counts.items():
+    if bigcount is None or count>bigcount:
+        bigword = word
+        bigcount = count
+
+print(bigword, bigcount)
